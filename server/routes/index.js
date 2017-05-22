715ad1696ef462b7a8dc96d3bde8a792726f6ef8
Ashish Ranjan <gettheashish@gmail.com>
2017-05-23 01:27:34 +0530

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+
+router.get('/registration', function(req, res, next) {
+  
+  	path = 'registration.html';
+  
+  return res.sendfile(path, {root: './client'});
+});
 /* GET home page. */
 router.get('*', function(req, res, next) {
   if(req.user != undefined){
